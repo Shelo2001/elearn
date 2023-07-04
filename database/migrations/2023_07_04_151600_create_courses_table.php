e36c81a42043a19d9a464d9b983b837c4723bcb4
Shelo2001 <sheliaakaki47@gmail.com>
2023-07-04 19:25:46 +0400

    added course and ratings migration

---
+++ database/migrations/2023_07_04_151600_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('duration_minutes');
+            $table->integer('price');
+            $table->integer('category');
+            $table->boolean('is_published')->default(false);
+            $table->unsignedBigInteger('author')->unsigned();
+            $table->foreign('author')->references('id')->on('users')->onDelete('cascade');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
